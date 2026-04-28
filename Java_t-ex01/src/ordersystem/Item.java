307114bdf38e156ee9f06fe6dcef4e2f482a79f9
24yn0129 <24yn0129@jec.ac.jp>
2026-04-28 10:01:03 +0900

    Item.java を更新

--- Java_t-ex01/src/ordersystem/Item.java
+++ Java_t-ex01/src/ordersystem/Item.java
@@ -1,18 +1,19 @@
 package ordersystem;
-/**
- * Itemクラス.
- */
-public class Item {
 
-	private String name;
-	private int price;
-	
-	Item(String name,int price) {
-		this.name = name;
-		this.price = price;
-	}
-	
-	static void show() {
-		//System.out.println(name + ":\\" + price);
-	}
+public class Item {
+    private String name;
+    private int price;
+
+    // コンストラクタ
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // 商品名と価格を表示するメソッド
+    public void show() {
+        // ※指定通り、半角の円マーク（バックスラッシュ）を表示させるために「\\」とエスケープしています
+        System.out.println(this.name + ":\\" + this.price);
+    }
 }
+
