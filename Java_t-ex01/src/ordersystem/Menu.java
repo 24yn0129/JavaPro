e4e29daf317417e4ad98abd7059891b4046b40de
24yn0129 <24yn0129@jec.ac.jp>
2026-04-28 10:02:13 +0900

    Menu.java を更新

--- Java_t-ex01/src/ordersystem/Menu.java
+++ Java_t-ex01/src/ordersystem/Menu.java
@@ -4,19 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
+    private List<Item> itemList;
 
-	List<Item> itemList = new ArrayList<>();
+    // コンストラクタ
+    public Menu() {
+        // フィールド itemList を ArrayList で初期化します
+        this.itemList = new ArrayList<Item>();
+    }
 
-	Menu(){
-		
-	}
+    // メニューに商品を追加するメソッド
+    public void add(Item item) {
+        this.itemList.add(item);
+    }
 
-	static void add(Item item) {
-
-	}
-
-	static void show() {
-
-	}
-
+    // メニューを表示するメソッド
+    public void show() {
+        System.out.println("********************************");
+        System.out.println("★取扱いメニュー★");
+        
+        // メニューに含まれる商品の数だけループします
+        for (int i = 0; i < this.itemList.size(); i++) {
+            // インデックス番号（行頭の数字）と空白を改行なしで表示します
+            System.out.print(i + " ");
+            // Itemクラスのshowメソッドを呼び出して、商品名と価格を表示（ここで改行されます）
+            this.itemList.get(i).show();
+        }
+        
+        System.out.println("********************************");
+    }
 }
+
