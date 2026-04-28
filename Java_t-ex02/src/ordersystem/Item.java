89c9795c6688bb9ccb2039d313c432a6d672296d
24yn0129 <24yn0129@jec.ac.jp>
2026-04-28 10:03:04 +0900

    Item.java を更新

--- Java_t-ex02/src/ordersystem/Item.java
+++ Java_t-ex02/src/ordersystem/Item.java
@@ -1,11 +1,21 @@
-/**
- * 
- */
 package ordersystem;
 
-/**
- * 
- */
 public class Item {
+    private String name;
+    private int price;
 
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // 追加：価格を取得するゲッター
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void show() {
+        System.out.println(this.name + ":\\" + this.price);
+    }
 }
+
