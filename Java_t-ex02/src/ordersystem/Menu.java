665355e5f3d257888ac136e403df3e9d5036b769
24yn0129 <24yn0129@jec.ac.jp>
2026-04-28 10:03:22 +0900

    Menu.java を更新

--- Java_t-ex02/src/ordersystem/Menu.java
+++ Java_t-ex02/src/ordersystem/Menu.java
@@ -1,5 +1,32 @@
 package ordersystem;
 
-public class Menu {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Menu {
+    private List<Item> itemList;
+
+    public Menu() {
+        this.itemList = new ArrayList<Item>();
+    }
+
+    public void add(Item item) {
+        this.itemList.add(item);
+    }
+
+    // 追加：指定された商品番号の商品を返すメソッド
+    public Item getItem(int itemNo) {
+        return this.itemList.get(itemNo);
+    }
+
+    public void show() {
+        System.out.println("********************************");
+        System.out.println("★取扱いメニュー★");
+        for (int i = 0; i < this.itemList.size(); i++) {
+            System.out.print(i + " ");
+            this.itemList.get(i).show();
+        }
+        System.out.println("********************************");
+    }
 }
+
