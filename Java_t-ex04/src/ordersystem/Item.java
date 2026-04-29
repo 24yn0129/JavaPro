2080e49829e738dd672ed4a0d229ef9671a68a99
24yn0129 <24yn0129@jec.ac.jp>
2026-04-29 23:40:29 +0900

    Item.java を更新

--- Java_t-ex04/src/ordersystem/Item.java
+++ Java_t-ex04/src/ordersystem/Item.java
@@ -4,14 +4,14 @@ package ordersystem;
  * 商品を表すクラスです。
  */
 public class Item {
-    /** 商品名 */
+    // 商品名
     private String name;
-    /** 価格 */
+    // 価格
     private int price;
 
     /**
      * 指定された商品名と価格でItemを初期化します。
-     * * @param name  商品名
+     * @param name  商品名
      * @param price 価格
      */
     public Item(String name, int price) {
@@ -21,17 +21,14 @@ public class Item {
 
     /**
      * 商品の価格を取得します。
-     * * @return 商品の価格
+     * @return 商品の価格
      */
     public int getPrice() {
         return this.price;
     }
 
-    /**
-     * 商品名と価格をコンソールに表示します。
-     */
+    // 商品名と価格をコンソールに表示します。
     public void show() {
         System.out.println(this.name + ":\\" + this.price);
     }
 }
-
