84779f0766ea197e1a92562365da36bce5eaee40
24yn0129 <24yn0129@jec.ac.jp>
2026-04-29 23:41:04 +0900

    Order.java を更新

--- Java_t-ex04/src/ordersystem/Order.java
+++ Java_t-ex04/src/ordersystem/Order.java
@@ -1,11 +1,46 @@
-/**
- * 
- */
 package ordersystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 
+ * 注文された商品を管理するクラスです。
  */
 public class Order {
+    // 注文された商品のリスト
+    private List<Item> orderedItemList;
 
+    // Orderを初期化します。
+    public Order() {
+        this.orderedItemList = new ArrayList<Item>();
+    }
+
+    /**
+     * 注文リストに商品を追加します。
+     * @param item 追加する商品
+     */
+    public void add(Item item) {
+        this.orderedItemList.add(item);
+    }
+
+    /**
+     * 注文された商品の合計金額を計算して取得します。
+     * @return 合計金額
+     */
+    public int getTotalAmount() {
+        int total = 0;
+        for (Item item : this.orderedItemList) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    // 注文された商品の一覧と合計金額を表示します。
+    public void show() {
+        for (Item item : this.orderedItemList) {
+            item.show();
+        }
+        System.out.println("合計金額:\\" + this.getTotalAmount());
+    }
 }
+
